b7f5a3739f433c7948ae55211bb59e17249f3d27
Farid Shahy <fshahy@gmail.com>
2021-02-05 23:04:57 +0330

    add restaurant service basic logger.

---
+++ restaurants/src/logger/index.ts
@@ -0,0 +1,15 @@
+import winston, { createLogger, format, transports } from 'winston';
+
+const { combine, timestamp, printf } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+    return `[${level}] ${timestamp}: ${message}`;
+});
+
+export const logger = createLogger({
+    format: combine(
+        timestamp(),
+        logFormat
+    ),
+    transports: [new transports.Console()]
+});
