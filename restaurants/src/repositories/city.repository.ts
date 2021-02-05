1b553a6f9580eaa7bfc7f6ab30672e2559ea720f
Farid Shahy <fshahy@gmail.com>
2021-02-05 23:04:08 +0330

    add restaurant service repositories.

---
+++ restaurants/src/repositories/city.repository.ts
@@ -0,0 +1,6 @@
+import { Repository } from "typeorm";
+import { City } from '../entity/city.entity';
+
+export class CityRepository extends Repository<City> {
+
+}
---
+++ restaurants/src/repositories/food.repository.ts
@@ -0,0 +1,6 @@
+import { Repository } from 'typeorm';
+import { Food } from '../entity/food.entity';
+
+export class FoodRepository extends Repository<Food> {
+
+}
